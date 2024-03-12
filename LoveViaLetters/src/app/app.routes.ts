12cc2d18c9015287ceb4995b78f3c2748340462f
brian <brianriordan6466@gmail.com>
2024-03-12 18:51:09 +0000

    Temp Routing

--- LoveViaLetters/src/app/app.routes.ts
+++ LoveViaLetters/src/app/app.routes.ts
@@ -1,10 +1,20 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
+import {CreateProfileComponent} from "./create-profile/create-profile.component";
 
 export const routes: Routes = [
   {
     path: "login",
     component: LoginComponent
+  },
+  {
+    path: "register",
+    component: RegisterComponent
+  },
+  {
+    path: "create-profile",
+    component: CreateProfileComponent
   },
 
 ];
