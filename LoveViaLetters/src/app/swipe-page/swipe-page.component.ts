5203f915c0f3432a6d67033fd7bcea026ce21242
silpac-12 <seancaplis@gmail.com>
2024-03-13 18:32:57 +0000

    Added very basic swipe page

---
+++ LoveViaLetters/src/app/swipe-page/swipe-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-swipe-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './swipe-page.component.html',
+  styleUrl: './swipe-page.component.css'
+})
+export class SwipePageComponent {
+
+}
