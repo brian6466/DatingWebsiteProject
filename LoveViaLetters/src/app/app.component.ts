30ffa580be60878652e0f3046c847ad389649d7e
silpac-12 <seancaplis@gmail.com>
2024-03-25 21:20:30 +0000

    blank

--- LoveViaLetters/src/app/app.component.ts
+++ LoveViaLetters/src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {HeaderComponent} from "./header/header.component";
+//import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+
+
+
+
+
 
 @Component({
   selector: 'app-root',
--- LoveViaLetters/src/app/login/login.component.ts
+++ LoveViaLetters/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,5 +12,19 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  email: String = "";
+  password: String = "";
+
+  constructor(private router: Router) {
+
+  }
+
+  showLogin() {
+
+    console.log("User Email: ", this.email);
+    console.log("User Password: ", this.password);
+  }
+
 
 }
